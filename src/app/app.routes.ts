4e554f859843fdd8e6e1d79f75478593f7794af1
Andrés C. Marín <andriu0901@gmail.com>
2026-02-03 21:49:31 -0500

    adjusting css and new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { ReportsComponent } from './pages/reports/reports.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AddComponent } from './pages/reports/add/add.component';
 
 export const routes: Routes = [
     {
         path: 'reports',
         component: ReportsComponent
+    },
+    {
+        path: 'reports/add',
+        component: AddComponent
     },
     {
         path: '',
--- src/app/pages/reports/reports.component.ts
+++ src/app/pages/reports/reports.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { LucideAngularModule, Search, Plus, ChevronDown, ChevronUp, ChevronRight, ChevronLeft, ChartPieIcon, PencilIcon, TrashIcon } from "lucide-angular";
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, LucideAngularModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
+  readonly SearchIcon = Search;
+  readonly PlusIcon = Plus;
+  readonly ChevronDownIcon = ChevronDown;
+  readonly ChevronUpIcon = ChevronUp;
+  readonly ChevronRightIcon = ChevronRight;
+  readonly ChevronLeftIcon = ChevronLeft;
+  readonly ChartPieIcon = ChartPieIcon;
+  readonly PencilIcon = PencilIcon;
+  readonly TrashIcon = TrashIcon;
 
+  isNewest: boolean = true;
+
+  listReports: { title: string, created: string, modified: string, owner: string, form: string, active?: boolean }[] = [
+    {
+      title: "Test report",
+      created: "2022-01-01",
+      modified: "2022-01-01",
+      owner: "John Doe",
+      form: "Test form",
+      active: true
+    },
+    {
+      title: "Test report",
+      created: "2026-01-01",
+      modified: "2026-02-01",
+      owner: "Andres Castaneda",
+      form: "Test A"
+    }
+  ]
+
+  toggleSort() {
+    this.isNewest = !this.isNewest;
+  }
+
+  selectReport(selectedReport: { title: string, created: string, modified: string, owner: string, form: string, active?: boolean }) {
+    this.listReports.forEach(report => report.active = false);
+
+    selectedReport.active = true;
+  }
 }
